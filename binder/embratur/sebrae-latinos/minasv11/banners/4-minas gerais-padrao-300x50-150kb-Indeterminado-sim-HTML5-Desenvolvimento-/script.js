1b564a4c88a84480e31d2cbbf1b52d67aa55a569
CristianOliveiraS <eduardoliverasuos@gmail.com>
2025-09-22 17:29:45 -0300

    minas v11

---
+++ binder/embratur/sebrae-latinos/minasv11/banners/4-minas gerais-padrao-300x50-150kb-Indeterminado-sim-HTML5-Desenvolvimento-/script.js
@@ -0,0 +1,124 @@
+var animations = [
+      {
+    action: function() {
+        var confettiDrop = [
+          [
+            {
+              id: '#i01',
+              duration: 250,
+              delayBefore: 0,
+              delayAfter: 0,
+              animations: {opacity:1, scale:[1.25,1.25]}
+            },
+            {
+              id: '#i01',
+              duration: 5000,
+              delayBefore: 0,
+              delayAfter: 0,
+              animations: {scale:[1,1.25]}
+            },
+          ],
+        ];
+        var confettiAnim = new WAnimation(confettiDrop, {loop: false, clearAfterEnd: false});
+        confettiAnim.startAnimation();
+    }
+  },
+  [ 
+    {
+        id: '#i02',
+        duration: 650,
+        delayBefore: 250,
+        delayAfter: 0,
+        easing: 'easeOutCubic',
+        animations: {opacity: 1, left:0, top:0},   
+    },
+    {
+        id: '#i03',
+        duration: 650,
+        delayBefore: 450,
+        delayAfter: 0,
+        easing: 'easeOutCubic',
+        animations: {opacity: 1, left:0, top:0},   
+    },
+    {
+        id: '#i04',
+        duration: 650,
+        delayBefore: 550,
+        delayAfter: 3000,
+        easing: 'easeOutCubic',
+        animations: {opacity: 1, left:0, top:0},   
+    },
+  ],
+        {
+    action: function() {
+        var confettiDrop = [
+          [
+            {
+              id: '#i05',
+              duration: 250,
+              delayBefore: 0,
+              delayAfter: 0,
+              animations: {opacity:1, scale:[1.25,1.25]}
+            },
+            {
+              id: '#i05',
+              duration: 5000,
+              delayBefore: 0,
+              delayAfter: 0,
+              animations: {scale:[1,1.25]}
+            },
+          ],
+        ];
+        var confettiAnim = new WAnimation(confettiDrop, {loop: false, clearAfterEnd: false});
+        confettiAnim.startAnimation();
+    }
+  },
+  [ 
+  {
+        id: '#i06',
+        duration: 650,
+        delayBefore: 450,
+        delayAfter: 0,
+        easing: 'easeOutCubic',
+        animations: {opacity: 1, left:0, top:0},   
+    },
+    {
+        id: '#i07',
+        duration: 650,
+        delayBefore: 550,
+        delayAfter: 3000,
+        easing: 'easeOutCubic',
+        animations: {opacity: 1, left:0, top:0},   
+    },
+  ],
+  [
+  {
+        id: '#i08',
+        duration: 350,
+        delayBefore: 250,
+        delayAfter: 5000,
+        easing: 'easeOutCubic',
+        animations: {opacity: 1, left:0, top:0},   
+    },
+      {
+        id: '#i09',
+        duration: 1050,
+        delayBefore: 450,
+        delayAfter: 0,
+        easing: 'easeOutCubic',
+        animations: {opacity: 1, left:0, top:0},   
+    },
+    {
+        id: '#i10',
+        duration: 1050,
+        delayBefore: 550,
+        delayAfter: 3000,
+        easing: 'easeOutCubic',
+        animations: {opacity: 1, left:0, top:0},   
+    },
+  ],
+ ]
+ new WAnimation(animations, {
+     loop: false,
+     clearAfterEnd: false,
+   }).startAnimation();      
