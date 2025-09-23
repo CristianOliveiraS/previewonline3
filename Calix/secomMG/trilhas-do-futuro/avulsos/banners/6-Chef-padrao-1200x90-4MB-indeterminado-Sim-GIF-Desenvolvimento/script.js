d35005d364de175d63f7408babcbf7ed449e8da3
CristianOliveiraS <eduardoliverasuos@gmail.com>
2025-09-23 08:40:11 -0300

    avulsos trilhas do futuro

---
+++ Calix/secomMG/trilhas-do-futuro/avulsos/banners/6-Chef-padrao-1200x90-4MB-indeterminado-Sim-GIF-Desenvolvimento/script.js
@@ -0,0 +1,76 @@
+var animations = [
+  [
+    {
+      id: '#i01',
+      duration: 250,
+      delayBefore: 250,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, left:0, top:0}
+    },   
+    {
+      id: '#i02',
+      duration: 650,
+      delayBefore: 350,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, left:0, top:0}
+    },  
+    {
+      id: '#i03',
+      duration: 650,
+      delayBefore: 450,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, left:0, top:0}
+    },  
+    {
+      id: '#i04',
+      duration: 650,
+      delayBefore: 550,
+      delayAfter: 3000,
+      easing: "easeOutCubic",
+      animations: {opacity:1, left:0, top:0}
+    },    
+  ],
+  [
+  {
+      id: '#i05',
+      duration: 450,
+      delayBefore: 250,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, left:0, top:0}
+    }, 
+    {
+      id: '#i06',
+      duration: 650,
+      delayBefore: 350,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, left:0, top:0}
+    },  
+    {
+      id: '#i07',
+      duration: 650,
+      delayBefore: 450,
+      delayAfter: 3000,
+      easing: "easeOutCubic",
+      animations: {opacity:1, left:0, top:0}
+    },  
+  ],
+  [
+  {
+      id: '#i08',
+      duration: 550,
+      delayBefore: 250,
+      delayAfter: 3000,
+      easing: "easeOutCubic",
+      animations: {opacity:1, left:0, top:0}
+    }, 
+  ],
+ ]
+ new WAnimation(animations, {
+     loop: true,
+     clearAfterEnd: false,
+   }).startAnimation();      
