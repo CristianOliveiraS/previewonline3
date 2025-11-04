7293af150af95fc93f924d8b66d90b768a072f9c
CristianOliveiraS <eduardoliverasuos@gmail.com>
2025-11-04 15:39:29 -0300

    alemanha V2

---
+++ Calia/embratur/soft-power/ALEMANHA/completoV2/banners/10-Alemanha-padrao-300x600-250kb-15s-sim-html-Desenvolvimento-/script.js
@@ -0,0 +1,215 @@
+var animations = [
+  {
+    action: function () {
+      var confettiDrop = [
+        [
+          {
+            id: "#i01",
+            duration: 250,
+            delayBefore: 0,
+            delayAfter: 0,
+            animations: { opacity: 1, scale: [1.25, 1.25] },
+          },
+          {
+            id: "#i01",
+            duration: 5000,
+            delayBefore: 0,
+            delayAfter: 0,
+            animations: { scale: [1, 1.25] },
+          },
+        ],
+      ];
+      var confettiAnim = new WAnimation(confettiDrop, {
+        loop: false,
+        clearAfterEnd: true,
+      });
+      confettiAnim.startAnimation();
+    },
+  },
+  [
+    {
+      id: "#i02",
+      duration: 650,
+      delayBefore: 250,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+    {
+      id: "#i03",
+      duration: 650,
+      delayBefore: 450,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+    {
+      id: "#i04",
+      duration: 650,
+      delayBefore: 550,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+    {
+      id: "#i05",
+      duration: 650,
+      delayBefore: 650,
+      delayAfter: 3000,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+  ],
+  {
+    action: function () {
+      var confettiDrop = [
+        [
+          {
+            id: "#i06",
+            duration: 250,
+            delayBefore: 0,
+            delayAfter: 0,
+            animations: { opacity: 1, scale: [1.25, 1.25] },
+          },
+          {
+            id: "#i06",
+            duration: 5000,
+            delayBefore: 0,
+            delayAfter: 0,
+            animations: { scale: [1, 1.25] },
+          },
+        ],
+      ];
+      var confettiAnim = new WAnimation(confettiDrop, {
+        loop: false,
+        clearAfterEnd: true,
+      });
+      confettiAnim.startAnimation();
+    },
+  },
+  [
+    {
+      id: "#i07",
+      duration: 650,
+      delayBefore: 250,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+    {
+      id: "#i08",
+      duration: 650,
+      delayBefore: 450,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+    {
+      id: "#i08-1",
+      duration: 650,
+      delayBefore: 550,
+      delayAfter: 2500,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+  ],
+  {
+    action: function () {
+      var confettiDrop = [
+        [
+          {
+            id: "#i09",
+            duration: 250,
+            delayBefore: 0,
+            delayAfter: 0,
+            animations: { opacity: 1, scale: [1.25, 1.25] },
+          },
+          {
+            id: "#i09",
+            duration: 5000,
+            delayBefore: 0,
+            delayAfter: 0,
+            animations: { scale: [1, 1.25] },
+          },
+        ],
+      ];
+      var confettiAnim = new WAnimation(confettiDrop, {
+        loop: false,
+        clearAfterEnd: true,
+      });
+      confettiAnim.startAnimation();
+    },
+  },
+  [
+    {
+      id: "#i10",
+      duration: 650,
+      delayBefore: 250,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+    {
+      id: "#i11",
+      duration: 650,
+      delayBefore: 450,
+      delayAfter: 2500,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+  ],
+  [
+    {
+      id: "#i12",
+      duration: 350,
+      delayBefore: 250,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+    {
+      id: "#i13",
+      duration: 650,
+      delayBefore: 450,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+    {
+      id: "#i14",
+      duration: 650,
+      delayBefore: 550,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+    {
+      id: "#i14-1",
+      duration: 650,
+      delayBefore: 650,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+    {
+      id: "#i15",
+      duration: 650,
+      delayBefore: 850,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+    {
+      id: "#i16",
+      duration: 650,
+      delayBefore: 950,
+      delayAfter: 4000,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+  ],
+];
+new WAnimation(animations, {
+  loop: true,
+  clearAfterEnd: false,
+}).startAnimation();
