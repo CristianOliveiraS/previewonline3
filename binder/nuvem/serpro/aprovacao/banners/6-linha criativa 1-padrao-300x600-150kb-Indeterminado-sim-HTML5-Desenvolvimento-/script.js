f07fd56093ca216012f3070df7f393ff67af4588
CristianOliveiraS <eduardoliverasuos@gmail.com>
2025-11-14 20:06:18 -0300

    binder nuvem aprovacao

---
+++ binder/nuvem/serpro/aprovacao/banners/6-linha criativa 1-padrao-300x600-150kb-Indeterminado-sim-HTML5-Desenvolvimento-/script.js
@@ -0,0 +1,103 @@
+var animations = [
+  {
+    action: function () {
+      var confettiDrop = [
+        [
+          {
+            id: "#i01",
+            duration: 250,
+            delayBefore: 0,
+            delayAfter: 0,
+            animations: { opacity: 1, scale: [1.25, 1.25] },
+          },
+          {
+            id: "#i01",
+            duration: 8000,
+            delayBefore: 0,
+            delayAfter: 0,
+            animations: { scale: [1, 1.25] },
+          },
+        ],
+      ];
+      var confettiAnim = new WAnimation(confettiDrop, {
+        loop: false,
+        clearAfterEnd: false,
+      });
+      confettiAnim.startAnimation();
+    },
+  },
+  [
+    {
+      id: "#i02",
+      duration: 650,
+      delayBefore: 350,
+      delayAfter: 1000,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, top: 0, left: 0 },
+    },
+  ],
+  [
+    {
+      id: "#i03",
+      duration: 650,
+      delayBefore: 350,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, top: 0, left: 0 },
+    },
+    {
+      id: "#i04",
+      duration: 650,
+      delayBefore: 450,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, top: 0, left: 0 },
+    },
+    {
+      id: "#i05",
+      duration: 650,
+      delayBefore: 650,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, top: 0, left: 0 },
+    },
+    {
+      id: "#i06",
+      duration: 650,
+      delayBefore: 750,
+      delayAfter: 3000,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, top: 0, left: 0 },
+    },
+  ],
+  [
+    {
+      id: "#i07",
+      duration: 250,
+      delayBefore: 0,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, top: 0, left: 0 },
+    },
+    {
+      id: "#i08",
+      duration: 750,
+      delayBefore: 350,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, top: 0, left: 0 },
+    },
+    {
+      id: "#i09",
+      duration: 750,
+      delayBefore: 450,
+      delayAfter: 3000,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, top: 0, left: 0 },
+    },
+  ],
+];
+new WAnimation(animations, {
+  loop: true,
+  clearAfterEnd: false,
+}).startAnimation();
