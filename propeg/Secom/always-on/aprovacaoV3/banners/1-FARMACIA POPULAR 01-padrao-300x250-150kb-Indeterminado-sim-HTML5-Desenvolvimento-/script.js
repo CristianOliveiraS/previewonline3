4835eb50e548c38ccc6a62e9441b2280545c5521
CristianOliveiraS <eduardoliverasuos@gmail.com>
2025-11-28 11:43:27 -0300

    banner secom

---
+++ propeg/Secom/always-on/aprovacaoV3/banners/1-FARMACIA POPULAR 01-padrao-300x250-150kb-Indeterminado-sim-HTML5-Desenvolvimento-/script.js
@@ -0,0 +1,156 @@
+var animations = [
+  {
+    action: function () {
+      var confettiDrop = [
+        [
+          {
+            id: "#i01",
+            duration: 250,
+            delayBefore: 0,
+            delayAfter: 0,
+            animations: { opacity: 1 },
+          },
+          {
+            id: "#i01",
+            duration: 15050,
+            delayBefore: 0,
+            delayAfter: 0,
+            animations: { scale: [1.25, 1] },
+          },
+        ],
+      ];
+      var confettiAnim = new WAnimation(confettiDrop, {
+        loop: false,
+        clearAfterEnd: false,
+      });
+      confettiAnim.startAnimation();
+    },
+  },
+  [
+    {
+      id: "#i02",
+      duration: 750,
+      delayBefore: 350,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+    {
+      id: "#i03",
+      duration: 750,
+      delayBefore: 450,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+    {
+      id: "#i04",
+      duration: 750,
+      delayBefore: 850,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+    {
+      id: "#i05",
+      duration: 750,
+      delayBefore: 950,
+      delayAfter: 3000,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+  ],
+  [
+    {
+      id: "#i06",
+      duration: 250,
+      delayBefore: 150,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+    {
+      id: "#i07",
+      duration: 750,
+      delayBefore: 450,
+      delayAfter: 3000,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+  ],
+    {
+    action: function () {
+      var confettiDrop = [
+        [
+          {
+            id: "#i08",
+            duration: 250,
+            delayBefore: 0,
+            delayAfter: 0,
+            animations: { opacity: 1 },
+          },
+          {
+            id: "#i08",
+            duration: 15050,
+            delayBefore: 0,
+            delayAfter: 0,
+            animations: { scale: [1.25, 1] },
+          },
+        ],
+      ];
+      var confettiAnim = new WAnimation(confettiDrop, {
+        loop: false,
+        clearAfterEnd: false,
+      });
+      confettiAnim.startAnimation();
+    },
+  },
+  [
+    {
+      id: "#i09",
+      duration: 750,
+      delayBefore: 350,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+    {
+      id: "#i10",
+      duration: 750,
+      delayBefore: 450,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+    {
+      id: "#i11",
+      duration: 750,
+      delayBefore: 750,
+      delayAfter: 3000,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+  ],
+  [
+   {
+      id: "#i12",
+      duration: 250,
+      delayBefore: 150,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    },
+    {
+      id: "#i13",
+      duration: 750,
+      delayBefore: 350,
+      delayAfter: 3000,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, left: 0, top: 0 },
+    }, 
+  ]
+];
+new WAnimation(animations, {
+  loop: false,
+  clearAfterEnd: false,
+}).startAnimation();
