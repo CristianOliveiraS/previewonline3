7c2bdcb91d20b3a4ec1c40047138407e590346ec
CristianOliveiraS <eduardoliverasuos@gmail.com>
2025-12-16 23:45:14 -0300

    normal e digitado sebrae

---
+++ Debrito/sebrae/banners-sebrae/aprovacao-digitado/banners/3-Rosa-padrao-728x90-150kb-indeterminado-Sim-HTML-Desenvolvimento-/script.js
@@ -0,0 +1,492 @@
+var animations = [
+    {
+    action: function() {
+        var confettiDrop = [
+          [
+            {
+              id: '#i01',
+              duration: 250,
+              delayBefore: 0,
+              delayAfter: 0,
+              animations: {opacity:1, scale:[1.25,1.25]}
+            },
+            {
+              id: '#i01',
+              duration: 500,
+              delayBefore: 0,
+              delayAfter: 0,
+              animations: {scale:[1,1.25]}
+            },
+          ],
+        ];
+        var confettiAnim = new WAnimation(confettiDrop, {loop: false, clearAfterEnd: false});
+        confettiAnim.startAnimation();
+    }
+  },
+  [ 
+    {
+      id: '#i02',
+      duration: 650,
+      delayBefore: 350,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i03',
+      duration: 650,
+      delayBefore: 450,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i04',
+      duration: 650,
+      delayBefore: 650,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i05',
+      duration: 650,
+      delayBefore: 750,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i06',
+      duration: 650,
+      delayBefore: 850,
+      delayAfter: 3000,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+  ],
+  [
+    {
+      id: '#i07',
+      duration: 250,
+      delayBefore: 250,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08',
+      duration: 650,
+      delayBefore: 450,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-1',
+      duration: 650,
+      delayBefore: 550,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-2',
+      duration: 650,
+      delayBefore: 650,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-3',
+      duration: 650,
+      delayBefore: 750,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-4',
+      duration: 650,
+      delayBefore: 850,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-5',
+      duration: 650,
+      delayBefore: 950,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-6',
+      duration: 650,
+      delayBefore: 1050,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-7',
+      duration: 650,
+      delayBefore: 1150,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-8',
+      duration: 650,
+      delayBefore: 1250,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-9',
+      duration: 650,
+      delayBefore: 1350,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-10',
+      duration: 650,
+      delayBefore: 1450,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-11',
+      duration: 650,
+      delayBefore: 1550,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-12',
+      duration: 650,
+      delayBefore: 1650,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-13',
+      duration: 650,
+      delayBefore: 1750,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-14',
+      duration: 650,
+      delayBefore: 1850,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-15',
+      duration: 650,
+      delayBefore: 1950,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-16',
+      duration: 650,
+      delayBefore: 2050,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-17',
+      duration: 650,
+      delayBefore: 2150,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-18',
+      duration: 650,
+      delayBefore: 2250,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-19',
+      duration: 650,
+      delayBefore: 2350,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-20',
+      duration: 650,
+      delayBefore: 2450,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-21',
+      duration: 650,
+      delayBefore: 2550,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-22',
+      duration: 650,
+      delayBefore: 2650,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-23',
+      duration: 650,
+      delayBefore: 2750,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-24',
+      duration: 650,
+      delayBefore: 2850,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-25',
+      duration: 650,
+      delayBefore: 2950,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-26',
+      duration: 650,
+      delayBefore: 3050,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-27',
+      duration: 650,
+      delayBefore: 3150,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-28',
+      duration: 650,
+      delayBefore: 3250,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-29',
+      duration: 650,
+      delayBefore: 3350,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-30',
+      duration: 650,
+      delayBefore: 3450,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-31',
+      duration: 650,
+      delayBefore: 3550,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-32',
+      duration: 650,
+      delayBefore: 3650,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-33',
+      duration: 650,
+      delayBefore: 3750,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-34',
+      duration: 650,
+      delayBefore: 3850,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-35',
+      duration: 650,
+      delayBefore: 3950,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-36',
+      duration: 650,
+      delayBefore: 4050,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-37',
+      duration: 650,
+      delayBefore: 4150,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-38',
+      duration: 650,
+      delayBefore: 4250,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-39',
+      duration: 650,
+      delayBefore: 4350,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-40',
+      duration: 650,
+      delayBefore: 4450,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-41',
+      duration: 650,
+      delayBefore: 4550,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-42',
+      duration: 650,
+      delayBefore: 4650,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-43',
+      duration: 650,
+      delayBefore: 4750,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-44',
+      duration: 650,
+      delayBefore: 4850,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-45',
+      duration: 650,
+      delayBefore: 4950,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-46',
+      duration: 650,
+      delayBefore: 5050,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08-47',
+      duration: 650,
+      delayBefore: 5150,
+      delayAfter: 1000,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+  ],
+  [
+    {
+      id: '#i09',
+      duration: 250,
+      delayBefore: 250,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i10',
+      duration: 650,
+      delayBefore: 450,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i11',
+      duration: 650,
+      delayBefore: 650,
+      delayAfter: 3000,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+  ],
+ ]
+ new WAnimation(animations, {
+     loop: false,
+     clearAfterEnd: false,
+   }).startAnimation();      
