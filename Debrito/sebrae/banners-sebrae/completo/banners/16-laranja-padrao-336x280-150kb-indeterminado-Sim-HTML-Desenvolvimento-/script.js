af96a330c827e4d82aa7e2ec5d1de4491272b683
CristianOliveiraS <eduardoliverasuos@gmail.com>
2025-12-18 10:26:37 -0300

    sebrae compoleto 18/12/25

---
+++ Debrito/sebrae/banners-sebrae/completo/banners/16-laranja-padrao-336x280-150kb-indeterminado-Sim-HTML-Desenvolvimento-/script.js
@@ -0,0 +1,166 @@
+var animations = [
+  {
+    action: function () {
+      var confettiDrop = [
+        [
+          {
+            id: "#i01",
+            duration: 250,
+            delayBefore: 0,
+            delayAfter: 0,
+            animations: { opacity: 1 },
+          },
+          {
+            id: "#i01",
+            duration: 9000,
+            delayBefore: 0,
+            delayAfter: 0,
+            animations: { scale: [1.25, 1] },
+          },
+        ],
+      ];
+      var confettiAnim = new WAnimation(confettiDrop, {
+        loop: false,
+        clearAfterEnd: true,
+      });
+      confettiAnim.startAnimation();
+    },
+  },
+  [
+    {
+      id: "#i02",
+      duration: 650,
+      delayBefore: 1850,
+      delayAfter: 3000,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, top: 0, left: 0 },
+    },
+  ],
+  [
+    {
+      id: "#i03",
+      duration: 250,
+      delayBefore: 0,
+      delayAfter: 0,
+      animations: { opacity: 1 },
+    },
+    {
+      id: "#i04",
+      duration: 650,
+      delayBefore: 250,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, top: 0, left: 0 },
+    },
+    {
+      id: "#i05",
+      duration: 650,
+      delayBefore: 650,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, top: 0, left: 0 },
+    },
+    {
+      id: "#i06",
+      duration: 650,
+      delayBefore: 750,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, top: 0, left: 0 },
+    },
+    {
+      id: "#i07",
+      duration: 650,
+      delayBefore: 850,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, top: 0, left: 0 },
+    },
+    {
+      id: "#i08",
+      duration: 650,
+      delayBefore: 950,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, top: 0, left: 0 },
+    },
+    {
+      id: "#i08-1",
+      duration: 650,
+      delayBefore: 1050,
+      delayAfter: 3000,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, top: 0, left: 0 },
+    },
+  ],
+  [
+    {
+      id: "#i09",
+      duration: 250,
+      delayBefore: 0,
+      delayAfter: 0,
+      animations: { opacity: 1 },
+    },
+    {
+      id: "#i10",
+      duration: 650,
+      delayBefore: 250,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, top: 0, left: 0 },
+    },
+    {
+      id: "#i11",
+      duration: 650,
+      delayBefore: 650,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, top: 0, left: 0 },
+    },
+    {
+      id: "#i12",
+      duration: 650,
+      delayBefore: 650,
+      delayAfter: 4500,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, top: 0, left: 0 },
+    },
+  ],
+  [
+    {
+      id: "#i13",
+      duration: 250,
+      delayBefore: 0,
+      delayAfter: 0,
+      animations: { opacity: 1 },
+    },
+    {
+      id: "#i14",
+      duration: 650,
+      delayBefore: 250,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, top: 0, left: 0 },
+    },
+    {
+      id: "#i16",
+      duration: 650,
+      delayBefore: 650,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, top: 0, left: 0 },
+    },
+    {
+      id: "#i15",
+      duration: 650,
+      delayBefore: 850,
+      delayAfter: 3000,
+      easing: "easeOutCubic",
+      animations: { opacity: 1, top: 0, left: 0 },
+    },
+  ],
+];
+new WAnimation(animations, {
+  loop: false,
+  clearAfterEnd: false,
+}).startAnimation();
